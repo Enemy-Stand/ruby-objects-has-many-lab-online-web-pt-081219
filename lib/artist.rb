abfd26f991f4964c8492c0c14d28c3186e1aba79
Joseph Sesti <sestijoey63@gmail.com>
2019-09-16 17:59:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,16 +13,16 @@ class Artist
   end
   
   def add_song(song)
-    song.artist = self
+    Song.artist = self
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
-    song.artist = self
+    Song.artist = self
   end
   
   def songs
-    song.all.select {|song| song.artist == self}
+    Song.all.select {|song| song.artist == self}
   end
   
   def self.song_count
