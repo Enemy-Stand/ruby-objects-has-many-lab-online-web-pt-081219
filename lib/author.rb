a0de645f42d9567b2caaff019ec39363fe4f6883
Joseph Sesti <sestijoey63@gmail.com>
2019-09-16 18:02:41 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,31 @@
 class Author
-  attr_accessor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_()
+     = self
+  end
+  
+  def add_song_by_name(name)
+    song = Post.new(name)
+    post.author = self
+  end
+  
+  def songs
+    Post.all.select {|| . == self}
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
+end
