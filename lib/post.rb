462b464160a7f4d8a81ca783318ce5c88c85be99
Joseph Sesti <sestijoey63@gmail.com>
2019-09-16 18:04:42 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+  attr_accessor :name, :author
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if author
+      author.name
+    else
+      nil
+    end
+  end
+end
